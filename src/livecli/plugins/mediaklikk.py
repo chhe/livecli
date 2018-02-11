d7c348feedc42e35101c182c1ae6d53211dc0494
back-to <backto@protonmail.ch>
2018-02-11 15:31:34 +0100

    [mediaklikk] Fixed plugin livestream, vod and radio

--- src/livecli/plugins/mediaklikk.py
+++ src/livecli/plugins/mediaklikk.py
@@ -1,7 +1,6 @@
 import re
 
 from livecli.plugin import Plugin
-from livecli.stream import HLSStream
 from livecli.plugin.api import http
 
 __livecli_docs__ = {
@@ -13,40 +12,37 @@ __livecli_docs__ = {
     ],
     "notes": "",
     "live": True,
-    "vod": False,
-    "last_update": "2016-06-10",
+    "vod": True,
+    "last_update": "2018-02-11",
 }
-
-_stream_url_re = re.compile(r"http(s)?://(www\.)?mediaklikk.hu/([A-Za-z0-9\-]+)/?")
-_id_re = re.compile(r'.*data\-streamid="([a-z0-9]+)".*')
-_file_re = re.compile(r'.*{\'file\': ?\'([a-zA-Z0-9\./\?=:]+)\'}].*')
-
-_stream_player_url = "http://player.mediaklikk.hu/player/player-inside-full3.php?userid=mtva&streamid={0}&flashmajor=21&flashminor=0"
 
 
 class Mediaklikk(Plugin):
+
+    _url_re = re.compile(r"http(s)?://(www\.)?mediaklikk\.hu/")
+    _id_re = re.compile(r"""(?P<q>["'])(?:streamId|token)(?P=q):(?P=q)(?P<id>[^"']+)(?P=q)""")
+
+    new_self_url = "https://player.mediaklikk.hu/playernew/player.php?video={0}"
+
     @classmethod
     def can_handle_url(cls, url):
-        return _stream_url_re.match(url)
-
-    def _get_playlist_url(self):
-        # get the id
-        content = http.get(self.url)
-        match = _id_re.match(content.text.replace("\n", ""))
-        if not match:
-            return
-
-        # get the m3u8 file url
-        player_url = _stream_player_url.format(match.group(1))
-        content = http.get(player_url)
-
-        match = _file_re.match(content.text.replace("\n", ""))
-        if match:
-            return match.group(1)
+        return cls._url_re.match(url)
 
     def _get_streams(self):
-        playlist = self._get_playlist_url()
-        return HLSStream.parse_variant_playlist(self.session, playlist)
+        res = http.get(self.url)
+
+        m = self._id_re.search(res.text)
+        if not m:
+            self.logger.info("Found no videoid.")
+            self.url = "resolve://{0}".format(self.url)
+            return self.session.streams(self.url)
+
+        video_id = m.group("id")
+        if video_id:
+            self.logger.debug("Found id: {0}".format(video_id))
+            self.url = self.new_self_url.format(video_id)
+
+            return self.session.streams(self.url)
 
 
 __plugin__ = Mediaklikk
