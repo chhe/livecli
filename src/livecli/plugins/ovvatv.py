6075d27259658396d9e5d8b00691465183a791bf
back-to <backto@protonmail.ch>
2018-02-14 23:58:47 +0100

    [ovvatv] Fix for new domain, resolve.py will now search for the iframe

--- src/livecli/plugins/ovvatv.py
+++ src/livecli/plugins/ovvatv.py
@@ -1,36 +1,36 @@
-from __future__ import print_function
 import re
 
 from base64 import b64decode
+from datetime import datetime
 
 from livecli import PluginError
 from livecli.plugin import Plugin
 from livecli.plugin.api import http
-from livecli.plugin.api import useragents
 from livecli.plugin.api import validate
 from livecli.stream import HLSStream
-from livecli.compat import urlparse
 from livecli.utils import parse_json
 
 __livecli_docs__ = {
     "domains": [
-        "ovva.tv",
+        "1plus1.video",
     ],
-    "geo_blocked": [],
+    "geo_blocked": [
+        "UA",
+    ],
     "notes": "",
     "live": True,
     "vod": False,
-    "last_update": "2017-02-16",
+    "last_update": "2018-02-14",
 }
 
 
 class ovvaTV(Plugin):
-    url_re = re.compile(r"https?://(?:www\.)?ovva.tv/(?:ua/)?tvguide/.*?/online")
-    iframe_re = re.compile(r"iframe .*?src=\"((?:https?:)?//(?:\w+\.)?ovva.tv/[^\"]+)\"", re.DOTALL)
-    data_re = re.compile(r"ovva\(\'(.*?)\'\);")
+    url_re = re.compile(r"https?://(?:www\.)?1plus1\.video/tvguide/embed/[^/]")
+    data_re = re.compile(r"""ovva-player["'],["'](.*?)["']\)};""")
+    next_date_re = re.compile(r"""<div\sclass=["']o-message-timer['"]\sdata-timer=["'](\d+)["']""")
     ovva_data_schema = validate.Schema({
-        "url": validate.url()
-    }, validate.get("url"))
+        "balancer": validate.url()
+    }, validate.get("balancer"))
     ovva_redirect_schema = validate.Schema(validate.all(
         validate.transform(lambda x: x.split("=")),
         ['302', validate.url()],
@@ -41,33 +41,23 @@ class ovvaTV(Plugin):
     def can_handle_url(cls, url):
         return cls.url_re.match(url) is not None
 
-    def find_iframe(self, res):
-        for url in self.iframe_re.findall(res.text):
-            if url.startswith("//"):
-                p = urlparse(self.url)
-                return "{0}:{1}".format(p.scheme, url)
-            else:
-                return url
-
     def _get_streams(self):
-        http.headers = {"User-Agent": useragents.ANDROID}
         res = http.get(self.url)
-        iframe_url = self.find_iframe(res)
-
-        if iframe_url:
-            self.logger.debug("Found iframe: {0}", iframe_url)
-            res = http.get(iframe_url, headers={"Referer": self.url})
-            data = self.data_re.search(res.text)
-            if data:
-                try:
-                    ovva_url = parse_json(b64decode(data.group(1)).decode("utf8"), schema=self.ovva_data_schema)
-                    stream_url = http.get(ovva_url, schema=self.ovva_redirect_schema)
-                except PluginError as e:
-                    self.logger.error("Could not find stream URL: {0}", e)
-                else:
-                    return HLSStream.parse_variant_playlist(self.session, stream_url)
+        data = self.data_re.search(res.text)
+        next_date = self.next_date_re.search(res.text)
+        if data:
+            try:
+                ovva_url = parse_json(b64decode(data.group(1)).decode("utf8"), schema=self.ovva_data_schema)
+                stream_url = http.get(ovva_url, schema=self.ovva_redirect_schema)
+            except PluginError as e:
+                self.logger.error("Could not find stream URL: {0}", e)
             else:
-                self.logger.error("Could not find player data.")
+                return HLSStream.parse_variant_playlist(self.session, stream_url)
+        elif next_date:
+            self.logger.info("The broadcast will be available at {0}".format(
+                datetime.fromtimestamp(int(next_date.group(1))).strftime('%Y-%m-%d %H:%M:%S')))
+        else:
+            self.logger.error("Could not find player data.")
 
 
 __plugin__ = ovvaTV
