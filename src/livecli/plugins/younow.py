d72b16a0d84eb1afdcc9f70f28200abc271adcab
back-to <backto@protonmail.ch>
2018-02-16 13:52:59 +0100

    [younow] cleanup python code

--- src/livecli/plugins/younow.py
+++ src/livecli/plugins/younow.py
@@ -1,5 +1,3 @@
-"""Plugin for younow.com by WeinerRinkler"""
-
 import re
 
 from livecli.plugin import Plugin
@@ -14,52 +12,41 @@ __livecli_docs__ = {
     "notes": "",
     "live": True,
     "vod": False,
-    "last_update": "2015-07-21",
+    "last_update": "2018-02-16",
 }
-
-jsonapi = "https://api.younow.com/php/api/broadcast/info/curId=0/user="
-
-# http://younow.com/channel/
-_url_re = re.compile(r"http(s)?://(\w+.)?younow.com/(?P<channel>[^/&?]+)")
-
-
-def getStreamURL(channel):
-    url = jsonapi + channel
-    res = http.get(url)
-    streamerinfo = http.json(res)
-    # print(streamerinfo)
-
-    if not any("media" in s for s in streamerinfo):
-        print("User offline or invalid")
-        return
-    else:
-        streamdata = streamerinfo['media']
-        # print(streamdata)
-        streamurl = "rtmp://" + streamdata['host'] + streamdata['app'] + "/" + streamdata['stream']
-        # print (streamurl)
-
-    return streamurl
 
 
 class younow(Plugin):
+
+    _url_re = re.compile(r"https?://(?:\w+\.)?younow\.com/(?P<channel>[^/&?]+)")
+
+    api_url = "https://api.younow.com/php/api/broadcast/info/curId=0/user={0}"
+
     @classmethod
-    def can_handle_url(self, url):
-        return _url_re.match(url)
+    def can_handle_url(cls, url):
+        return cls._url_re.match(url)
 
     def _get_streams(self):
-        match = _url_re.match(self.url)
+        match = self._url_re.match(self.url)
         channel = match.group("channel")
 
-        streamurl = getStreamURL(channel)
-        if not streamurl:
-            return
-        streams = {}
-        streams["live"] = RTMPStream(self.session, {
-            "rtmp": streamurl,
-            "live": True
-        })
+        res = http.get(self.api_url.format(channel))
+        data = http.json(res)
 
-        return streams
+        if data.get("errorCode") == 0:
+            media = data.get("media")
+            if media:
+                rtmp_url = "rtmp://{host}{app}/{stream}".format(
+                    host=media["host"],
+                    app=media["app"],
+                    stream=media["stream"],
+                )
+                params = {
+                    "rtmp": rtmp_url,
+                    "live": True
+                }
+
+                yield "live", RTMPStream(self.session, params=params)
 
 
 __plugin__ = younow
